0880d2dbaa615c7f9853e5ba481da2d26f12b5b0
Lukas Stockburger <lukas.stockburger@googlemail.com>
2017-06-13 10:12:10 +0200

    first commit

---
+++ src/layer-ui-adapter.js
@@ -0,0 +1,12 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDom from 'react-dom';
+import * as Layer from 'layer-websdk';
+import * as LayerUI from 'layer-ui-web';
+
+// initialize lauerUI with your appID and layer sdk
+LayerUI.init({
+  appId: window.layerSample.appId,
+});
+
+const LayerUIWidgets = LayerUI.adapters.react(React, ReactDom);
+module.exports = LayerUIWidgets;
